6a9c822bc452e41c4686d36976d663763607f548
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-31 13:45:14 +0900

    [feat] feature1 기능 구현

--- app/src/main/java/org/sopt/gitlab/MainActivity.kt
+++ app/src/main/java/org/sopt/gitlab/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // feature1 기능 구현
     }
 }
