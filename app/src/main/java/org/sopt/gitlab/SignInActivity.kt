b8814192b483ba5fd64f50586c993daa6caaa174
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-01 23:06:17 +0900

    main에 작업추가

--- app/src/main/java/org/sopt/gitlab/SignInActivity.kt
+++ app/src/main/java/org/sopt/gitlab/SignInActivity.kt
@@ -1,11 +1,12 @@
 package org.sopt.gitlab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        // main에 작업 추가
     }
 }
