c8d681ab3e50d5da6438cdc6179847f876d6969c
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-01 23:07:59 +0900

    rebase 에 작업완료

--- app/src/main/java/org/sopt/gitlab/SignUpActivity.kt
+++ app/src/main/java/org/sopt/gitlab/SignUpActivity.kt
@@ -7,5 +7,6 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        // test2에서 작업 끝남 이제 main에 이거 합칠거임~
     }
 }
